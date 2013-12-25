044fe5849e71975d7bf2e208bb8ad5698f1eb017
Alex Ravet <ravet.alexandre@gmail.com>
2013-12-25 23:13:56 +0100

    initial commit

---
+++ Alex-portfolio/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlexPortfolio::Application.config.secret_key_base = '8f025f60cd83106828aeda380c07f5845b73ea11d536f79e9017229e354baa19fb6ee9795b60d99267e6c9c90261fa2d7ffa5e9b26fbb45e7bc3471ed9623014'
